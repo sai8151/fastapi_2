a6695b061ef917a86e3f87d7f34c0e8c13500210
sai8151 <saikiranreddy8151@gmail.com>
2024-07-31 14:08:15 +0530

    #w

--- notes_app/database.py
+++ notes_app/database.py
@@ -4,6 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/notes_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=100,  # Increase the pool size
+    max_overflow=200,  # Increase the overflow size
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+#uvicorn notes_app.main:app --reload
